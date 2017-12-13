714ed3ad5cf8e8205841e1c5b3c6a2f7327df0b4
navid <nbahmanyar@zoho.com>
2017-12-12 19:11:16 -0600

    working on movie website

---
+++ entertainment_center.py
@@ -0,0 +1,27 @@
+import media
+import fresh_tomatoes
+
+thin_blue_line = media.Movie("Thin Blue Line",
+							 "Crime Documentary",
+							 "https://upload.wikimedia.org/wikipedia/en/thumb/6/69/The_Thin_Blue_Line_poster.jpg/220px-The_Thin_Blue_Line_poster.jpg",
+							 "https://www.youtube.com/watch?v=oEb_2mj1V8w"
+							)
+thin_blue_line = media.Movie("Thin Blue Line",
+							 "Crime Documentary",
+							 "https://upload.wikimedia.org/wikipedia/en/thumb/6/69/The_Thin_Blue_Line_poster.jpg/220px-The_Thin_Blue_Line_poster.jpg",
+							 "https://www.youtube.com/watch?v=oEb_2mj1V8w"
+							)
+thin_blue_line = media.Movie("Thin Blue Line",
+							 "Crime Documentary",
+							 "https://upload.wikimedia.org/wikipedia/en/thumb/6/69/The_Thin_Blue_Line_poster.jpg/220px-The_Thin_Blue_Line_poster.jpg",
+							 "https://www.youtube.com/watch?v=oEb_2mj1V8w"
+							)
+thin_blue_line = media.Movie("Thin Blue Line",
+							 "Crime Documentary",
+							 "https://upload.wikimedia.org/wikipedia/en/thumb/6/69/The_Thin_Blue_Line_poster.jpg/220px-The_Thin_Blue_Line_poster.jpg",
+							 "https://www.youtube.com/watch?v=oEb_2mj1V8w"
+							)
+
+movie_list = [thin_blue_line]
+
+fresh_tomatoes.open_movies_page(movie_list)
